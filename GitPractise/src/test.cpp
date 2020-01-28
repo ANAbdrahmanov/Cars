1b42cd5d7ed690aa11919dba3f77177f7048055d
abdrakhm@k104636 <aydar.abdrakhmanov@mera.com>
2020-01-28 16:58:50 +0300

    some task 2 conflicting action

--- GitPractise/src/test.cpp
+++ GitPractise/src/test.cpp
@@ -8,24 +8,30 @@
 
 #define CARS_COUNT 10
 
-void Race(const std::vector<std::shared_ptr<Car>>& cars) {
-	for (int i = 0; i < 5; i++) {
+void Race(const std::vector<std::shared_ptr<Car>>& cars) 
+{
+	for (int i = 0; i < 5; i++) 
+	{
 		std::cout << "Round " << i + 1 << std::endl;
-		for (auto it = cars.begin(); it != cars.end(); ++it) {
+		for (auto it = cars.begin(), end = cars.end(); it != end; ++it)
+		{
 			(*it)->Drive();
 		}
 		std::cout << "\n";
 	}
 }
 
-int main() {
+int main() 
+{
 	std::vector<std::shared_ptr<Car>> cars;
 	std::shared_ptr<IPublisher> pRadio = std::make_shared<Radio>();
 	auto randomCarFactory = std::unique_ptr<CarFactory>(new CRandomCarFactory());
-	for (int i = 0; i < CARS_COUNT; i++) {
+	for (int i = 0; i < CARS_COUNT; i++) 
+	{
 		cars.push_back(randomCarFactory->CreateCar(pRadio));
 	}
 	Race(cars);
 	std::cin.get();
+	std::cout << "some conflicting action and some convention fixes" << "\n";
 	return 0;
 }
